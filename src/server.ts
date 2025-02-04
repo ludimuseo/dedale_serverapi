a3c8fd9e5383d5a2b1aa97bfa2c4ed2e3dfb7dc3
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2025-02-04 15:57:19 +0100

    💥 NEW: Add middleware errorHandler.ts

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import stepsRouter from "./routes/steps.routes";
 import piecesRouter from "./routes/pieces.routes";
 import medalsRouter from "./routes/medals.routes";
 import usersRouter from "./routes/users.routes";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 app.use(express.json());
@@ -22,6 +23,7 @@ app.use("/steps", stepsRouter);
 app.use("/pieces", piecesRouter);
 app.use("/medals", medalsRouter);
 app.use("/users", usersRouter);
+app.use(errorHandler);
 
 
 //--------------------------------------------------------------
