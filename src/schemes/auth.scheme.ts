2df7740524f2b2a130ff6adb921ef79861124310
fred2541 <fredionze@free.fr>
2025-02-04 11:31:15 +0100

    Add auth.scheme

---
+++ src/schemes/auth.scheme.ts
@@ -0,0 +1,37 @@
+const { DataTypes} = require("sequelize");
+const Sequelize = require("../server.ts");
+
+const Auth = Sequelize.define("auth", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+      },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false
+      },
+    log: {
+      type: DataTypes.STRING,
+      allowNull: true
+      },
+    creatAt: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+      }
+      },
+        {
+          freezeTableName: true, // Empêche Sequelize d'ajouter un "s" à "user"
+          timestamps: false // Dont add createdAt and updatedAt in the query
+ });
+
+ export default Auth;
