646ade8f03dd0d9a6d1acc0860a11b4af1f1b030
fred2541 <fredionze@free.fr>
2025-02-04 12:23:23 +0100

    Add piece scheme

---
+++ src/schemes/piece.scheme.ts
@@ -0,0 +1,29 @@
+const { DataTypes} = require("sequelize");
+const Sequelize = require("../server.ts");
+
+const Piece = Sequelize.define("piece", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    isPublished: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+      },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+      }
+      },
+        {
+          freezeTableName: true, // Empêche Sequelize d'ajouter un "s" à la fin du nom de la table
+          timestamps: false // Dont add createdAt and updatedAt in the query
+ });
+
+ export default Piece;
