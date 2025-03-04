7334769fca607ba8e8c5ddc5f1c244d7be145146
fred2541 <fredionze@free.fr>
2025-03-04 17:48:35 +0100

    Add Active/Unactive place

--- src/routes/places.routes.ts
+++ src/routes/places.routes.ts
@@ -5,6 +5,7 @@ import {
   getAllPlaces,
   getPlaceById,
   createPlace,
+  activePlace,
   updatePlace,
   deletePlace,
 } from '../controllers/places.controller';
@@ -18,6 +19,7 @@ router.get('/list', getAllPlaces);
 router.post('/', auth, noMobile, ...validateLieuCreation, createPlace);
 router.get('/find/:id', validateId, getPlaceById);
 router.post('/create', auth, noMobile, createPlace); // Ajouter des validations supplémentaires si nécessaire
+router.put('/active', auth, noMobile, activePlace);
 router.patch('/update/:id', auth, noMobile, validateId, updatePlace); // Ajouter des validations supplémentaires si nécessaire
 router.delete('/delete/:id', auth, noMobile, validateId, deletePlace);
 
