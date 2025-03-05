7fb2fe5a31373c49b997733041c6480a6daa70e7
fred2541 <fredionze@free.fr>
2025-03-05 18:23:38 +0100

    :bug:fix(user login): disable authlog for user login(bug introduced)

--- src/services/auth_log.service.ts
+++ src/services/auth_log.service.ts
@@ -4,6 +4,7 @@ import { AuthenticatedRequest } from '../utils/types';
 export class AuthLog {
   static async save(req: AuthenticatedRequest, raison?: string) {
     const timestamp: number = Math.floor(Date.now() / 1000);
+
     await Auth_Log.create({
       login_attempt: timestamp,
       ip_adresse: req.ip || 'undefined',
--- src/services/users_login.service.ts
+++ src/services/users_login.service.ts
@@ -6,7 +6,7 @@ import { AuthenticatedRequest } from '../utils/types';
 
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { AuthLog } from './auth_log.service';
+// import { AuthLog } from './auth_log.service';
 
 const RANDOM_TOKEN_SECRET = process.env.RANDOM_TOKEN_SECRET;
 const TOKEN_EXPIRES_IN = process.env.TOKEN_EXPIRES_IN;
@@ -20,6 +20,7 @@ export class UsersLoginService {
     let data;
     // @ts-ignore
     let userID;
+    // @ts-ignore
     let reason;
     let isMatch;
 
@@ -91,7 +92,7 @@ export class UsersLoginService {
 
     success ? (successStatus = 'success') : (successStatus = 'failure');
 
-    await AuthLog.save(req, reason);
+    // await AuthLog.save(req, reason);
 
     if (data) {
       return data;
