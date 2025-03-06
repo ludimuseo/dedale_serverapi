390efd7c9ec4c32dd772e28df000765eb222cb62
GJMCharles <91600327+GJMCharles@users.noreply.github.com>
2025-03-06 22:38:29 +0100

    :recycle:refactor(eslint config): change 'eslint.config.js' to 'eslint.config.ts'

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,7 +4,6 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default tseslint.config(
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   {
@@ -14,15 +13,13 @@ export default tseslint.config(
         sourceType: 'module',
         ecmaVersion: 'latest',
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        // tsconfigRootDir: import.meta.dirname,
       },
-    },
-    plugins: {
-      prettier: eslintPrettierPlugin,
     },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
-  eslintPrettierConfig
+  eslintPrettierConfig,
+  eslintPrettierPlugin
 );
